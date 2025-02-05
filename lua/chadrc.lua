59db7e54bbd84e6505479aac90113dbdeecd0bb2
Andrei Lahunovich <lagunmelman@gmail.com>
2025-02-05 17:35:30 +0300

    change nvconfig

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,31 +1,12 @@
 -- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
-
 ---@type ChadrcConfig
 local M = {}
-
 
 M.base46 = {
   theme = "material-deep-ocean",
   transparency = true,
-
-  statusline = {
-    theme = "vscode_colored",
-  },
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      "██████╗ ██╗   ██╗██████╗ ██████╗ ██╗     ███████╗",
-      "██╔══██╗██║   ██║██╔══██╗██╔══██╗██║     ██╔════╝",
-      "██████╔╝██║   ██║██████╔╝██████╔╝██║     █████╗  ",
-      "██╔═══╝ ██║   ██║██╔══██╗██╔═══╝ ██║     ██╔══╝  ",
-      "██║     ╚██████╔╝██║  ██║██║     ███████╗███████╗",
-      "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝╚══════╝",
-    },
-  },
-
-
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
@@ -44,5 +25,65 @@ M.base46 = {
   },
 }
 
+M.ui = {
+  statusline = {
+    enabled = true,
+    theme = "default",
+    separator_style = "arrow",
+  },
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      "                            ",
+      "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+
+      "   ▄▀███▄     ▄██ █████▀    ",
+      "   ██▄▀███▄   ███           ",
+      "   ███  ▀███▄ ███           ",
+      "   ███    ▀██ ███           ",
+
+      "   ███      ▀ ███           ",
+      "   ▀██ █████▄▀█▀▄██████▄    ",
+      "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+      "                            ",
+
+      "     Powered By  eovim    ",
+      "                            ",
+    },
+
+    buttons = {
+      { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+      { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+
+      { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+      { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
+      { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
+
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+      {
+        txt = function()
+          local stats = require("lazy").stats()
+
+          local ms = math.floor(stats.startuptime) .. " ms"
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        end,
+        hl = "NvDashFooter",
+        no_gap = true,
+      },
+
+      { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    },
+  },
+  cmp = {
+    icons_left = false, -- only for non-atom styles!
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    abbr_maxwidth = 60,
+    format_colors = {
+      tailwind = false, -- will work for css lsp too
+      icon = "󱓻",
+    },
+  },
+}
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
 
 -- Git
 map("n", "<leader>gl", ":Flog<CR>", { desc = "Git Log" })
