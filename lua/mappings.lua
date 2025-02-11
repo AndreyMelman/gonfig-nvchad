33f20d91553de4f01d1bd465b462c267b00323ba
Andrei Lahunovich <lagunmelman@gmail.com>
2025-02-11 14:36:06 +0300

    update lazy-lock and add mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,5 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
@@ -11,6 +9,7 @@ map("i", "jj", "<ESC>")
 map("n", "<leader>gl", ":Flog<CR>", { desc = "Git Log" })
 map("n", "<leader>gf", ":DiffviewFileHistory<CR>", { desc = "Git File History" })
 map("n", "<leader>gc", ":DiffviewOpen HEAD~1<CR>", { desc = "Git Last Commit" })
-map("n", "<leader>gt", ":DiffviewToggleFile<CR>", { desc = "Git File History" })
+map("n", "<leader>gz", ":DiffviewClose<CR>", { desc = "Git File Close" })
+map("n", "<leader>gs", ":Telescope git_status<CR>", { desc = "Git Stasus" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
